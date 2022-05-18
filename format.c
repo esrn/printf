b8af69f1db25ebea178cc9825737387a0df0e121
Kerim571 <103578580+Kerim571@users.noreply.github.com>
2022-05-18 17:48:39 +0200

    format.c Kerim

---
+++ format.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  int i=25;
+  float f=1.23;
+  char c='k';
+  char s[]="Kerim";
+  printf("int: %i , float: %f , char: %c , String: %s",i,f,c,s);
+  return 0;
+}
